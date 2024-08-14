a50803b207a94698fb4bb9bdc3b052c049fc9c6c
tasuboyz <95526476+tasuboyz@users.noreply.github.com>
2024-08-14 07:54:49 +0200

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -6,6 +6,11 @@ from aiogram.filters import CommandStart
 from aiohttp.web import Request, json_response
 import websockets
 import json
+from beem import Steem, Hive
+from beem.account import Account
+from beem.blockchain import Blockchain
+from beem.comment import Comment
+from beem.community import Communities, Community
 
 TOKEN = "6348055484:AAGhOv6sx5B4acQj-XUfRWV3kJz53FvioWs" #TasuAdmin
 
@@ -15,37 +20,54 @@ dp = Dispatcher()
 bot = Bot(token=TOKEN)
 
 WEB_SERVER_HOST = "127.0.0.1"
-# Port for incoming request from reverse proxy. Should be any available port
+
 WEB_SERVER_PORT = 5173
 
-# Path to webhook route, on which Telegram will send requests
 WEBHOOK_PATH = r"/webhook"
-# Secret key to validate requests from Telegram (optional)
+
 WEBHOOK_SECRET = "my-secret"
-# Base URL for webhook will be used to generate webhook URL for Telegram,
-# in this example it is used public DNS with HTTPS support
-app = "8dff-240b-10-5e2-1800-15ac-6891-773e-5182.ngrok-free.app"
+
+app = "3471-240b-10-5e2-1800-2cac-522d-da1c-3d29.ngrok-free.app"
 
 BASE_WEBHOOK_URL = f"https://{app}"
 
-# All handlers should be attached to the Router (or Dispatcher)
 router = Router()
+
+connected_clients = set()
+
+steem_node = "https://api.moecki.online"
+stm = Steem(node=steem_node)
+community = Communities(blockchain_instance=stm)
 
 @router.message(CommandStart())
 async def command_start_handler(message: types.Message) -> None:
     keyboard = post_link()
-    await message.answer("start", reply_markup=keyboard)
+    test_url = 'https://t.me/TasuAdmin_Bot/test_web_app'
+    await message.answer(test_url, reply_markup=keyboard)
 
 @router.callback_query(F.data == 'send_data')
-async def send_data(callback_query: types.CallbackQuery):
-    uri = f"wss://{app}/ws"
-    async with websockets.connect(uri) as websocket:
-        data = {
-            "data": "data"
-        }
-        await websocket.send(json.dumps(data))
-        response = await websocket.recv()
-        print(f"Risposta dal server: {response}")
+async def send_data_handler(callback_query: types.CallbackQuery):
+    data_to_send = "Hello from the bot!"
+    await bot.answer_callback_query(callback_query.id, text="Data sent!")
+
+    if connected_clients:
+        for client in connected_clients:
+            await client.send_json(data_to_send)
+
+async def websocket_handler(request):
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+
+    connected_clients.add(ws)
+    try:
+        async for msg in ws:
+            if msg.type == web.WSMsgType.TEXT:
+                # Gestione dei messaggi ricevuti se necessario
+                pass
+    finally:
+        connected_clients.remove(ws)
+    
+    return ws
 
 async def handle_post(request: Request):
     try:
@@ -56,34 +78,27 @@ async def handle_post(request: Request):
         tag = data.get('tag')
         dateTime = data.get('dateTime')
         
-        print(f"user_id: {userId}, Title: {title}, Description: {description}, Tag: {tag}, DateTime: {dateTime}")
+        text = f"Title: {title}, Description: {description}, Tag: {tag}, DateTime: {dateTime}"
+        await bot.send_message(userId, text)
+        #print(f"user_id: {userId}, Title: {title}, Description: {description}, Tag: {tag}, DateTime: {dateTime}")
         
         return web.json_response({'status': 'success', 'message': 'Dati ricevuti con successo'})
     except Exception as e:
         print(f"Errore durante la gestione della richiesta POST: {e}")
         return web.json_response({'status': 'error', 'message': 'Errore durante la gestione della richiesta POST'}, status=500)
 
-async def websocket_handler(request):
-    ws = web.WebSocketResponse()
-    await ws.prepare(request)
-
-    async for msg in ws:
-        if msg.type == web.WSMsgType.TEXT:
-            await ws.send_str(f"Message received: {msg.data}")
-        elif msg.type == web.WSMsgType.ERROR:
-            print(f"WebSocket connection closed with exception {ws.exception()}")
-
-    print("WebSocket connection closed")
-    return ws
-
 def post_link():
+    
     keyboard = []
     keyboard.append([types.InlineKeyboardButton(text="link", web_app=types.WebAppInfo(url=BASE_WEBHOOK_URL))])
+    #keyboard.append([types.InlineKeyboardButton(text="test", web_app=types.WebAppInfo(url=test_url))])
     keyboard.append([types.InlineKeyboardButton(text="send_data", callback_data='send_data')])
     keyboard = types.InlineKeyboardMarkup(inline_keyboard=keyboard)
     return keyboard
 
-process = True
+def get_steem_community(community_name):
+    result = community.search_title(community_name)
+    return result
 
 async def on_startup(bot: Bot) -> None:
     await bot.delete_webhook()
@@ -104,6 +119,7 @@ async def main():
         app.router.add_get('/', handle)
         app.router.add_static('/assets/', path='./dist/assets', name='assets')
         app.router.add_post('/post', handle_post)
+        # start_server = websockets.serve(send_data, WEB_SERVER_HOST, WEB_SERVER_PORT)
         app.router.add_get('/ws', websocket_handler)
         #app.router.add_post('/send', send_data)
         #app.router.add_get('/send', send_data)
