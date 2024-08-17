c4850f1b6649aa1674db0e4c0c177e2a3961a99e
tasuboyz <95526476+tasuboyz@users.noreply.github.com>
2024-08-17 17:19:07 +0200

    Add files via upload

--- Cur8 TWA/src/page/api/postAPI.ts
+++ Cur8 TWA/src/page/api/postAPI.ts
@@ -1,1 +1,59 @@
+import { URL } from '../setup/config';
 
+interface Post {
+  title: string;
+  description: string;
+  tag: string;
+  dateTime: string;
+  userId: number | null;
+}
+
+interface ApiResponse<T> {
+  data: T;
+  error?: string;
+}
+
+async function apiCall<T>(endpoint: string, method: string, data?: string): Promise<ApiResponse<T>> {
+  const headers = {
+    "accept": "application/json",
+    "authorization": "Bearer my-secret",
+    "Content-Type": "application/json"
+  };
+
+  try {
+    const response = await fetch(`${URL}${endpoint}`, {
+      method,
+      headers,
+      body: data ? JSON.stringify(data) : undefined
+    });
+
+    if (!response.ok) {
+      throw new Error('API call failed');
+    }
+
+    const responseData: T = await response.json();
+    return { data: responseData };
+  } catch (error: unknown) {
+    let errorMessage = 'An unknown error occurred';
+    if (error instanceof Error) {
+      errorMessage = error.message;
+    }
+    console.error('API call error:', errorMessage);
+    return { data: {} as T, error: errorMessage };
+  }
+}
+
+// API methods
+export const postAPI = {
+  submit: async (post: Post): Promise<ApiResponse<string>> => {
+    return apiCall('/post', 'POST', JSON.stringify(post));
+  },
+
+  searchCommunity: async (communityId: string): Promise<ApiResponse<Array<{ id: string, name: string }>>> => {
+    return apiCall('/community', 'POST', JSON.stringify({ community: communityId }));
+  },
+
+  uploadImage: async (userId: number, imageBase64: string): Promise<ApiResponse<string>> => {
+    return apiCall('/image', 'POST', JSON.stringify({ userId, image: imageBase64 }));
+  }
+};
