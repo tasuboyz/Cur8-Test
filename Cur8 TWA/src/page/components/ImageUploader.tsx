ab74a547c38f20b2ec8c2891907bfd3598c1bfbf
tasuboyz <95526476+tasuboyz@users.noreply.github.com>
2024-08-17 17:17:28 +0200

    Add files via upload

--- Cur8 TWA/src/page/components/ImageUploader.tsx
+++ Cur8 TWA/src/page/components/ImageUploader.tsx
@@ -1,1 +1,27 @@
+import React, { ChangeEvent } from 'react';
 
+interface ImageUploaderProps {
+  onImageUpload: (imageBase64: string) => void;
+}
+
+const ImageUploader: React.FC<ImageUploaderProps> = ({ onImageUpload }) => {
+  const handleImageChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        const imageBase64 = reader.result as string;
+        onImageUpload(imageBase64);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleImageChange} />
+    </div>
+  );
+};
+
+export default ImageUploader;
