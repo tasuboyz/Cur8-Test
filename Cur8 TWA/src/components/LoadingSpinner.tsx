bf057d96b264c907b31caa2d0920b688c41ab212
tasuboyz <95526476+tasuboyz@users.noreply.github.com>
2024-08-17 17:13:13 +0200

    Add files via upload

--- Cur8 TWA/src/components/LoadingSpinner.tsx
+++ Cur8 TWA/src/components/LoadingSpinner.tsx
@@ -1,1 +1,9 @@
+const LoadingSpinner = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <div>Loading in progress...</div>
+    </div>
+  );
+};
 
+export default LoadingSpinner;
